ace5a0a304899908018008bdb71009ad9677dcb5
hadeelsala7 <h.salah.technical@gmail.com>
2021-08-18 23:31:07 +0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -27,4 +27,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function Department(){
+        return $this->belongsTo(Department::class,'department_id');
+    }
+
+    public function DepartmentManager(){
+        return $this->hasOne(Department::class,'manager_id');
+    }
+
+    public function Job(){
+        return $this->belongsTo(Job::class,'job_id');
+    }
+
+    public function JobHistory(){
+        return $this->hasMany(JobHistory::class,'user_id','id');
+    }
 }
