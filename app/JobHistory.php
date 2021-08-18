228815c2adb0216e50afb4d4a4596ea06d6512e4
hadeelsala7 <h.salah.technical@gmail.com>
2021-08-18 23:30:57 +0300

    Update JobHistory.php

--- app/JobHistory.php
+++ app/JobHistory.php
@@ -9,4 +9,17 @@ class JobHistory extends Model
 {
     //
     use SoftDeletes;
+
+
+    public function Employee(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+
+    public function Department(){
+        return $this->belongsTo(Department::class,'department_id');
+    }
+
+    public function Job(){
+        return $this->hasMany(Job::class,'job_id');
+    }
 }
