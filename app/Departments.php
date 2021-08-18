1c9e2ae4d8f96813bb52ba92be6932ac4887cbac
hadeelsala7 <h.salah.technical@gmail.com>
2021-08-18 17:57:17 +0300

    Create Departments.php

---
+++ app/Departments.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Departments extends Model
+{
+    //
+    use SoftDeletes;
+
+}
