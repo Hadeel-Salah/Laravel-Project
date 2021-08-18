cf7255ab6f9230215d8e2342547c8a78b0c9b24b
hadeelsala7 <h.salah.technical@gmail.com>
2021-08-18 23:30:50 +0300

    Update Departments.php

--- app/Departments.php
+++ app/Departments.php
@@ -11,4 +11,24 @@ class Departments extends Model
     //
     use SoftDeletes;
 
+    public function Location()
+    {
+        return $this->belongsTo(Location::class, 'location_id');
+    }
+
+    public function Employees()
+    {
+        return $this->hasMany(User::class, 'department_id', 'id');
+
+    }
+
+    public function Manager()
+    {
+        return $this->belongsTo(User::class, 'manager_id');
+
+    }
+
+    public function JobHistory(){
+        return $this->belongsTo(JobHistory::class,'department_id');
+    }
 }
