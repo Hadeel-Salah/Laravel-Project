1e62c68a34346b97e326e4c81723d70d85bae1bf
hadeelsala7 <h.salah.technical@gmail.com>
2021-08-18 17:57:56 +0300

    Create 2021_08_18_130757_create_locations_table.php

---
+++ database/migrations/2021_08_18_130757_create_locations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('street_address');
+            $table->string('city');
+            $table->string('postal_code')-> nullable();
+            $table->unsignedInteger('country_id');
+            $table->foreign('country_id')-> references('id')-> on('countries')->onDelete('cascade');
+
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
